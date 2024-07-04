c2844200ee876f48890c8f764052477ff5cd2426
aepuertol <andrespuertolara@gmail.com>
2024-07-04 18:11:15 -0500

    new commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from dash import Dash, dcc, html, dash_table, Input, Output, State, callback
+from dash import Dash, dcc, html, dash_table, Input, Output, State, callback,ctx
+from dash_extensions.enrich import Trigger, FileSystemCache
 
 import base64
 import datetime
@@ -7,11 +8,20 @@ import io
 import pandas as pd
 from langchain_community.document_loaders import PyPDFLoader
 from pypdf import PdfReader
+from utils import *
+from tqdm import tqdm
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
-
+ref_tools=pd.read_csv('reference_tools.csv')
+tools_list=ref_tools['software'].to_list()
+fsc = FileSystemCache("cache_dir")
+fsc1= FileSystemCache("cache_tools")
+fsc.set("progress", '0')
+fsc1.set("tools",'idle')
+#print(tools_list)
+'''
 app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
@@ -29,28 +39,186 @@ app.layout = html.Div([
             'textAlign': 'center',
             'margin': '10px'
         },
-        # Allow multiple files to be uploaded
-        multiple=True
+                
     ),
     html.Div(id='output-data-upload'),
+    html.Progress(id='loadbar',max=100),
+    html.Br(),
+    html.Button('Scan',id='scanner-button',n_clicks=0),
+    html.Div(id='msg-div'),
+    html.Div(id='tool-div'),
+    html.Div(id='vulnerabilities-div'),
+    dcc.Store(id='pdf_content'),
+    dcc.Interval(id='check-bar-interval',interval=500,n_intervals=0)
 ])
+'''
 
+app.layout = html.Div([
+    # Header
+    html.Div([
+        html.H1("Vulnerability Scanner", style={'textAlign': 'center', 'color': '#2C3E50'}),
+        html.Hr()
+    ], style={'padding': '10px', 'backgroundColor': '#ECF0F1'}),
+
+    # File Upload Section
+    html.Div([
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Arrastre aquí el PDF o  ',
+                html.A('Seleccione', style={'color': '#3498DB', 'textDecoration': 'underline'})
+            ]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '2px',
+                'borderStyle': 'dashed',
+                'borderRadius': '10px',
+                'textAlign': 'center',
+                'margin': '20px 0',
+                'backgroundColor': '#F2F3F4',
+                'cursor': 'pointer'
+            }
+        )
+    ], style={'textAlign': 'center'}),
+
+    # Output Section
+    html.Div(id='output-data-upload', style={'margin': '20px 0'}),
+
+    # Progress Bar
+    html.Div([
+        html.Progress(id='loadbar', max=100, style={'width': '100%', 'margin': '10px 0'}),
+        html.Br(),
+        html.Button('Escanear', id='scanner-button', n_clicks=0, style={
+            'width': '100%',
+            'backgroundColor': '#3498DB',
+            'color': 'white',
+            'border': 'none',
+            'borderRadius': '5px',
+            'cursor': 'pointer',
+            'fontSize': '16px'
+        })
+    ], style={'textAlign': 'center'}),
+
+    # Message and Tool Sections
+    html.Div(id='msg-div', style={'margin': '20px 0', 'padding': '10px', 'backgroundColor': '#F2F3F4', 'borderRadius': '5px'}),
+    html.Div(id='tool-div', style={'margin': '20px 0', 'padding': '10px', 'backgroundColor': '#F2F3F4', 'borderRadius': '5px'}),
+    html.Img(id='wordcloud-image'),
+    html.Div(id='vulnerabilities-div', style={'margin': '20px 0', 'padding': '10px', 'backgroundColor': '#F2F3F4', 'borderRadius': '5px'}),
+
+    # Hidden Storage and Interval Component
+    dcc.Store(id='pdf_content'),
+    dcc.Interval(id='check-bar-interval', interval=500, n_intervals=0)
+], style={'fontFamily': 'Arial, sans-serif', 'backgroundColor': '#ECF0F1', 'padding': '20px'})
+
+#this callback tracks which tool is being scanned during the process
+@app.callback(Output('tool-div','children'),
+              [Input('check-bar-interval','n_intervals')])
+def check_tool_progress(n):
+    value=fsc1.get('tools')
+    if value is None:
+        value='idle'
+    return value
+#this callback tracks the progress bar
+@app.callback(Output('loadbar','value'),
+              Output('loadbar','title'),
+             [Input('check-bar-interval','n_intervals')])
+def check_loadbar(n):
+    value=fsc.get("progress")
+    if value is None:
+        value='0'
+    
+    return value,f'{round(float(value),0)} %'
+
+# this callback is the pdf loading process, all the pdf contents are loaded to a dcc.Store
 @app.callback(Output('output-data-upload','children'),
+              Output('pdf_content','data'),
             [Input('upload-data','contents'),
              Input('upload-data','filename')])
 def get_data(contents,filename):
     print(filename)
     #print(contents)
     try:
-        encoded_content=contents[0].split(',')[1]
+        
+        return filename,contents
+
+
+        
+    except Exception as e:
+        print(e)
+ 
+
+    return '',[]
+# this callback is the main process
+@app.callback(Output('msg-div','children'),
+              Output('vulnerabilities-div','children'),
+              [Input('scanner-button','n_clicks'),
+               State('pdf_content','data')])
+def scan_doc(nclicks,pdfdata):
+    
+    if ctx.triggered_id=='scanner-button':
+        fsc.set("progress", '0')
+        try:
+            encoded_content=pdfdata.split(',')[1]
+        except:
+            return '',''
         pdf_buffer=io.BytesIO(base64.b64decode(encoded_content))
+        
         #loader = PyPDFLoader(pdf_buffer)
         reader = PdfReader(pdf_buffer)
         #
         #print(dir(reader))
-        page = reader.pages[20]
-        print(reader.get_num_pages())
-        #print(page.extract_text())
+        #page = reader.pages[20]
+        total_pages=reader.get_num_pages()
+        
+        print(f'Searching in {total_pages}')
+        big_str=''
+        print('Loading pdf file')
+        for n in tqdm(range(total_pages)):
+            #print(f'page {n+1}')
+            page=reader.pages[n]
+            page_text=page.extract_text()
+            big_str+=' '+page_text.lower()
+            progress=n*100/total_pages
+            
+            fsc.set("progress", f'{progress}')
+        fsc.set("progress", '100')
+        print('loaded')
+        print('looking for tools')
+        tools_found=[]
+        for word in tqdm(tools_list):
+            status=word_finder(word,big_str)
+            if status:
+                tools_found.append(word)
+        tools_found=list(set(tools_found))
+        print(tools_found)
+        
+        vulnerabilities_list=[]
+        for tool in tools_found:
+            fsc1.set("tools",f'Escaneando {tool} ..')
+           
+            df=get_vulnerability_dataframe(tool)
+            if df.shape[0]>0:
+                vulnerabilities_list.append(df)
+            else:
+                fsc1.set("tools",f'Hubo un problema escaneando {tool} ..')
+
+        fsc1.set("tools","Análisis Finalizado") 
+        if len(vulnerabilities_list)>0:   
+            all_vulnerabilities_df=pd.concat(vulnerabilities_list,ignore_index=True)
+            vul_data_table=create_datatable(all_vulnerabilities_df)
+            wordcloud_text=' '.join(all_vulnerabilities_df['Descripción'].to_list())
+            wordcloud_image=get_wordcloud(wordcloud_text)
+        else:
+            vul_data_table=''
+
+        
+        
+
+
+
+        
 
         #in case to save pdf file into assets
         #with open(f'assets/{filename[0]}','wb') as pdfile:
@@ -58,20 +226,8 @@ def get_data(contents,filename):
         #loader = PyPDFLoader(f'assets/{filename[0]}')
         #docs=loader.load_and_split()
         #print(docs[20])
-
-
-        
-    except Exception as e:
-        print(e)
-
-
-
-
-    
-    
-
-    return ''
-
+        return 'Documento cargado',vul_data_table
+    return '',''
 if __name__ == '__main__':
     app.run(debug=True)
 
---
+++ utils.py
@@ -0,0 +1,107 @@
+
+from dash import Dash, dcc, html, dash_table, Input, Output, State, callback,ctx
+import requests
+from datetime import datetime,timedelta,date
+import pandas as pd
+from dash import dash_table
+from PIL import Image
+from wordcloud import WordCloud,ImageColorGenerator
+from io import BytesIO
+
+
+def word_finder(word,text):
+
+    idx=text.find(word)
+    if idx!=-1:
+        return True
+    else:
+        return False
+    
+def get_vulnerability(match_word,date_ini,date_end):
+    
+    
+    date_string=f"pubStartDate={date_ini}&pubEndDate={date_end}"
+    page_string="startIndex=0"
+    url=f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={match_word}&{page_string}&{date_string}"
+    
+    try:
+        res2=requests.get(url).json()
+        #print(res2)
+        output=[{'contributor':vulnerability['cve']['sourceIdentifier'],\
+                'description':vulnerability['cve']['descriptions'],\
+                'date':vulnerability['cve']['published']} for vulnerability in res2['vulnerabilities']]
+    except:
+        output=[]            
+    return output
+
+def get_vulnerability_dataframe(keyword):
+    today=date.today().isoformat()
+    one_month_ago=(date.today() - timedelta(days=30)).isoformat()
+    date_ini=one_month_ago+'T00:00:00.000'
+    date_end=today+'T00:00:00.000'
+    vul=get_vulnerability(keyword,date_ini,date_end)
+    if len(vul)>0:
+        vul=vul[::-1]
+        vuln_df=pd.DataFrame()
+        for i,v in enumerate(vul):
+            for des in v['description']:
+                if des['lang']=='es':
+                    final_des=des['value']
+                    break
+                final_des=des['value']
+            vuln_df.loc[i,'Fecha']=v['date']
+            vuln_df.loc[i,'Autor']=v['contributor']
+            vuln_df.loc[i,'Descripción']=final_des
+        vuln_df['Herramienta']=keyword
+        return vuln_df
+    else:
+        return pd.DataFrame()
+
+
+
+def create_datatable(df):
+
+    datatable= dash_table.DataTable(
+        id='comments-table',
+        style_table={'height':'400px','overflowY':'auto','overflowX':'auto','width':'auto'},
+        style_header={'backgroundColor': '#393F56','fontWeight': 'bold','color':'white'},
+        export_format='xlsx',
+        page_size=20,
+        style_cell={'textAlign': 'left','font-family': 'Arial,Helvetica,sans-serif',
+                    'textOverflow': 'ellipsis',
+                    'overflow': 'hidden',
+                    'maxWidth': 0
+                    },
+        row_selectable="single",
+        columns=[{'name':i,'id':i} for i in df.columns],
+        filter_action="native",
+        tooltip_data=[
+        {
+            column: {'value': str(value), 'type': 'markdown'}
+            for column, value in row.items()
+        } for row in df.to_dict('records')
+    ],
+        data=df.to_dict('records')
+
+        
+    )
+
+    return datatable
+
+def get_wordcloud(text):
+    wordcloud = WordCloud(background_color="white",colormap='Oranges').generate(text)
+    img=wordcloud.to_image()
+    img_bytes = BytesIO()
+    img.save(img_bytes, format='PNG')
+    img_object='data:image/png;base64,{}'.format(base64.b64encode(img_bytes.getvalue()).decode())
+    return img_object
+    
+    
+
+
+        
+        
+
+
+
+
